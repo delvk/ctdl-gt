ff1ba8edd89d1b406113511704d962d374c26da9
delvk <nbkhuong97@gmail.com>
2017-04-15 21:48:37 +0700

    Add files via upload

--- processData.cpp
+++ processData.cpp
@@ -12,7 +12,6 @@
 using namespace std;
 
 #define GPS_DISTANCE_ERROR 0.005
-#define X 2
 inline bool operator==(BusInfo_t& lhs, char* rhs) {
 	return strcmp(lhs.id, rhs) == 0;
 }
@@ -37,6 +36,12 @@ inline bool operator < (BusInfo_t& lhs, BusInfo_t& rhs){
 inline bool operator > (BusInfo_t& lhs, BusInfo_t& rhs){
 	return (lhs.latitude > rhs.latitude);
 }
+inline bool operator >= (BusInfo_t& lhs, BusInfo_t& rhs){
+	return (lhs.latitude >= rhs.latitude);
+}
+inline bool operator <= (BusInfo_t& lhs, BusInfo_t& rhs){
+	return (lhs.latitude <= rhs.latitude);
+}
 bool initBusGlobalData(void** pGData) {
 	AVLTree<BusInfo_t> **a = new AVLTree<BusInfo_t>*();
 	a[0] = new AVLTree<BusInfo_t>();
@@ -51,49 +56,73 @@ void releaseBusGlobalData(void* pGData) {
 	a[0]->~AVLTree();
 	a[1]->~AVLTree();
 }
+void getID(char *des, int idx, char *source){
+	strcpy(des, source + idx*sizeof(char));
+}
 void swapLatLong(BusInfo_t &bus){
 	double temp = bus.latitude;
 	bus.latitude = bus.longitude;
 	bus.longitude = temp;
 }
+bool isPass(double &X, double &Y, double &delta){
+	return (Y - delta <= X && Y + delta >= X);
+}
+bool intervalVisit(double &a, double &b, AVLNode<BusInfo_t>* &root)
+{
+	if (root == NULL) return false;
+	if (root->data.latitude >= a && root->data.latitude <= b){
+		return true;
+		//intervalVisit(a, b, root->left);
+		//intervalVisit(a, b, root->right);
+	}
+	else if (root->data.latitude < a) return intervalVisit(a, b, root->right);
+	else return intervalVisit(a, b, root->left);
+}
 bool processEvent(busEvent_t& event, L1List<BusInfo_t>& bList, void* pGData) {
-	
-	AVLTree<BusInfo_t> **a = (AVLTree<BusInfo_t>**)pGData;
-	if (a[0]->getRoot() == NULL){
-		cout << "Emty tree" << endl;
+	/*************************THIS IS THE 2AVLTREE - myTree[0] save depend latlatide, other is longtitude******************/
+	AVLTree<BusInfo_t> **myTree = (AVLTree<BusInfo_t>**)pGData;
+	if (myTree[0]->getRoot() == NULL){
 		L1Item<BusInfo_t> *p = bList.getHead();
 		L1List<BusInfo_t> swapList;
 		while (p){
-			a[0]->insert(p->data);
+			myTree[0]->insert(p->data);
 			swapList.push_back(p->data);
 			p = p->pNext;
 		}
 		swapList.traverse(swapLatLong);
 		p = swapList.getHead();
-
-		/*for (int i = 0; i < 9; i++) {
-			cout << swapList[i].longitude << " "<<  swapList[i].latitude << endl;
-		}*/
 		while (p){
-			a[1]->insert(p->data);	
+			myTree[1]->insert(p->data);
 			p = p->pNext;
 			swapList.removeHead();
 		}
-		a[1]->traverseNLR(swapLatLong);
+		//myTree[1]->traverseNLR(swapLatLong);//Swap again to gain right value
 	}
-	//cout << *(BusInfo_t*)(pGData) << endl;
+	/************************************************* end 2 Tree**********************************************************/
 	//1AXXYXXXXX Y_lat Delta_lat
+	cout << event.code << ": ";
+	char busID[ID_MAX_LENGTH];
+	BusInfo_t temp;
 	switch (event.code[0])
 	{
 	case ('1') :
 	{
 				   if (event.code[1] == 'A'){
-					   cout << "1A" << endl;
-					   a[0]->traverseNLR(printBusInfo);
+					   AVLNode<BusInfo_t> *pR = myTree[0]->getRoot();
+					   getID(busID, 2, event.code);
+					   if (!bList.find(busID)) cout << "Failed" << endl;
+					   else{
+						   if (event.params[0] == NULL || event.params[1] == NULL) return false;
+						   double x_lat = event.params[0];
+						   double a = x_lat - event.params[1];
+						   double b = x_lat + event.params[1];
+						   if (intervalVisit(a, b, pR)) cout << "Succeed" << endl;
+						   else cout << "Failed" << endl;
+					   }
+					   return true;
 				   }
 				   else if (event.code[1] == 'B'){
 					   cout << "1B" << endl;
-					   a[1]->traverseNLR(printBusInfo);
 				   }
 				   else return false;
 				   return true;
